7fb7a16009915bc985c9b993daa49f697d8e9e56
alymdc29 <alymdc29@gmail.com>
2025-01-18 13:04:06 +0800

    Create variables for passwords to validate if they match

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inputField = document.querySelector('#input-field');
+const passwordField = document.querySelector('name="password');
+const confirmPasswordField = document.querySelector('name=confirm-password');
 
 inputField.addEventListener('input', () => {
   if (inputField.validity.valid) {
